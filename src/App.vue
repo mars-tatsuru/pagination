6138a7fba8047ca876286013ddea8ca65d8cba99
tatsuru koike <t-koike@marsflag.com>
2023-06-03 13:15:56 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// コンポネントの読み込み
+
+// vueのライブラリの読み込み
+import { RouterView } from "vue-router";
+import { onMounted, onBeforeMount, ref  } from "vue";
+
+
+</script>
+
+<template>
+  <router-view ></router-view>
+</template>
+
+<style scoped lang="scss">
+body{
+  background-color: #fff;
+  color: #000;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted, watch } from "vue";
+import Pagination from "@/components/Pagination.vue";
+
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped lang="scss">
+</style>
